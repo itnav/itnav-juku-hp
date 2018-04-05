f9cdc0e0a6f7d2feaa179b70083143414e9c57a3
Masaki Takeyama <takenoko000jp@gmail.com>
2018-04-05 15:57:03 +0900

    logoとnavbar表示

--- src/components/entry/Index.vue
+++ src/components/entry/Index.vue
@@ -1,9 +1,15 @@
 <template lang='pug'>
 .vue-index(:style='{"background-image": `url("${imgs[\"back\"]}")`}')
     .reactive-title {{ reactiveTitle() }}
-    common-navbar.is-light(:brand='title')
+    common-navbar.is-light(:brand-img='imgs["logo"]' :menus='menus')
 
-    section.main.section
+    section.main
+        section.hero
+            .hero-body.has-text-centered
+                h1: img.logo(:src='imgs["logo"]')
+
+        section.section
+
 </template>
 
 <script lang='ts'>
@@ -11,6 +17,7 @@ import { Vue, Component } from 'vue-property-decorator';
 import VueUtil from '@/scripts/util/VueUtil';
 import RootVue from '@/components/base/RootVue';
 import CommonNavbar from '@/components/common/CommonNavbar.vue';
+import { NavbarMenuItem } from '@/scripts/model/part/CommonNavbar';
 
 import Buefy from 'buefy';
 
@@ -21,11 +28,18 @@ Vue.use(Buefy);
  */
 @Component
 export default class Index extends RootVue {
-    public title = 'Sample';
+    public title = 'イトナブ塾';
+    public subtitle = 'HP';
     protected imgs = {
-        'back': require('@/resources/img/background.jpg')
+        'back': require('@/resources/img/background.jpg'),
+        'logo': require('@/resources/img/logo.png')
     };
-    public subtitle = 'Index';
+
+    protected menus = [
+        { pack: 'fa', icon: 'home', text: 'イトナブ塾とは', onClick: () => {
+            this.$snackbar.open('home');
+        } }
+    ] as NavbarMenuItem[];
 
     protected beforeCreate(): void {
         // Inner Vue 登録
@@ -38,8 +52,15 @@ export default class Index extends RootVue {
 @import 'all'
 
 .vue-index
+    width: 100%
+    height: 100%
+
     section.main
         max-width: 800px
         margin-left: auto
         margin-right: auto
+
+        img.logo
+            width: 100%
+            height: auto
 </style>
