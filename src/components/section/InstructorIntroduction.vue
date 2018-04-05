aef2bcd4b6ddc051024aa632f0c52465bbb41cae
Masaki Takeyama <takenoko000jp@gmail.com>
2018-04-05 21:32:26 +0900

    講師紹介レイアウト修正

--- src/components/section/InstructorIntroduction.vue
+++ src/components/section/InstructorIntroduction.vue
@@ -2,24 +2,26 @@
 .vue-instructor-introduction
     h2.title.has-text-centered 〜講師紹介〜
     .columns.is-multiline
-        .column.is-4(v-for='text in InstructorEntries')
-            img(:src='text.img')
-            p {{ text.nickname }}
-            p {{ text.name }}
-            p {{ text.position }}
-            p {{ text.description }}
+        .column.is-4(v-for='instructor in Instructors')
+            .instructor(:style='{ "background-color": instructor.color }')
+                img(:src='instructor.img')
+                p.has-text-centered.is-size-3 {{ instructor.nickname }}
+                p {{ instructor.name }}
+                p {{ instructor.position }}
+                p {{ instructor.description }}
 </template>
 
 <script lang='ts'>
 import { Vue, Component } from 'vue-property-decorator';
 import VueUtil from '@/scripts/util/VueUtil';
 
-type InstructorEntries = {
+type Instructor = {
     img: Imgs;
     nickname: string;
     name: string;
     position: string;
     description: string;
+    color: string;
 }
 
 /**
@@ -27,36 +29,40 @@ type InstructorEntries = {
  */
 @Component
 export default class InstructorIntroduction extends Vue {
-    protected InstructorEntries = [
+    protected Instructors = [
         {
             img: require('@/resources/img/tama.png'),
             nickname:'たま',
             name:'千葉 沙織',
             position:'イトナブ石巻スタッフ',
-            description:'ScratchやOzobotなどを使って小学生向けワークショップを行ったりしているScratchお姉さん'
+            description:'ScratchやOzobotなどを使って小学生向けワークショップを行ったりしているScratchお姉さん',
+            color: '#ff42f8'
         },
         {
             img: require('@/resources/img/uk.png'),
             nickname:'UK',
             name:'加藤 奨人',
             position:'イトナブ石巻スタッフ',
-            description:'イベント企画・運営マネージャーワークショップの内容を考えたり営業なども行っている。Scratchお兄さん'
+            description:'イベント企画・運営マネージャーワークショップの内容を考えたり営業なども行っている。Scratchお兄さん',
+            color: '#f84545'
         },
         {
             img: require('@/resources/img/yottu.png'),
             nickname:'よっつ',
             name:'金光 宏',
             position:'イトナブ石巻スタッフ',
-            description:'学生たちにプログラミングを教えたりWeb記事の作成、写真撮影などを行っている。'
+            description:'学生たちにプログラミングを教えたりWeb記事の作成、写真撮影などを行っている。',
+            color: '#4b42ff'
         },
         {
             img: require('@/resources/img/hinode.png'),
             nickname:'日の出',
             name:'武山 将己',
             position:'イトナブ石巻スタッフ',
-            description:'主に学生たちにプログラミングを教えるワークショップを開催したり、学校に出張授業に行っている。たまにイラストレーター'
+            description:'主に学生たちにプログラミングを教えるワークショップを開催したり、学校に出張授業に行っている。たまにイラストレーター',
+            color: '#ff873f'
         }
-    ] as InstructorEntries[];
+    ] as Instructor[];
 }
 </script>
 
@@ -64,4 +70,15 @@ export default class InstructorIntroduction extends Vue {
 @import 'variable'
 
 .vue-instructor-introduction
+    .instructor
+        height: 100%
+        padding: 1.25rem
+        max-width: 250px
+        margin: 0 auto
+        border-top-left-radius: 50% 26%
+        border-top-right-radius: 50% 26%
+        border-bottom-left-radius: 10% 5%
+        border-bottom-right-radius: 10% 5%
+        p
+            color: #ffffff
 </style>
