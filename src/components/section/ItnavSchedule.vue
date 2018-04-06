fbbdab27c7893f8d7ad74ab8e26827d59b10a41e
Masaki Takeyama <takenoko000jp@gmail.com>
2018-04-06 21:45:36 +0900

    ical.jsに直す

--- src/components/section/ItnavSchedule.vue
+++ src/components/section/ItnavSchedule.vue
@@ -1,6 +1,7 @@
 <template lang='pug'>
 .vue-itnav-schedule
     h2.title.has-text-centered 〜 スケジュール 〜
+    // ical.jsに直す
     google-calendar(calendar-id='7bppfo8fl0cm6d1hsqmfkjpn6o@group.calendar.google.com' :show-nav='true' :show-title='true')
 </template>
 
