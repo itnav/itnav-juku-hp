eab452c79ae3ddb3cad028841039fbf2ded1725d
it-brown <jonah4mjst@gmail.com>
2018-04-06 17:57:21 +0900

    create intro

--- src/components/section/ItnavIntro.vue
+++ src/components/section/ItnavIntro.vue
@@ -1,14 +1,15 @@
 <template lang='pug'>
 .vue-itnav-intro
     h2.title.has-text-centered 〜 イトナブとは 〜
+    img.margin-bottom(:src='imgs["hackathon"]')
 
-    img(:src='imgs["hackathon"]')
-
-    section.section.about.box.has-text-centered
+    .about.box.has-text-centered
         p イトナブとは「IT」×「遊ぶ」×「学ぶ」×「営む」×「イノベーション」の造語です。
         p 石巻の次世代を担う若者を対象にソフトウェア開発やグラフィックデザインを学ぶ拠点と機会を提供し、
         p 石巻から世界に挑戦できる環境づくりをしています。
-        button.button.is-rounded.is-primary more
+        a.more.button.is-primary
+            b-icon(icon='open-in-new')
+            span.text more
 </template>
 
 <script lang='ts'>
@@ -31,10 +32,22 @@ export default class ItnavIntro extends Vue {
 
 .vue-itnav-intro
     .about
-        margin-top: 3rem
-        margin-bottom: 3rem
-        max-width: 80%
+        margin-left: auto
+        margin-right: auto
+        max-width: 800px
 
         p:not(:last-child)
-            margin-bottom: 0.75rem
+            margin-bottom: 0.75em
+
+        a.more
+            padding-left: 1em
+            padding-right: 1em
+
+            span.icon
+                margin-right: 0.5em
+
+            span.text
+                $spacing: 0.25em
+                letter-spacing: $spacing
+                margin-right: -$spacing
 </style>
