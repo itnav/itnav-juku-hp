e06f973b771bb9a3a40c8ca3532a5e6bdfd67dfa
it-brown <jonah4mjst@gmail.com>
2018-04-09 17:23:06 +0900

    fix

--- src/components/section/ItnavAccess.vue
+++ src/components/section/ItnavAccess.vue
@@ -5,19 +5,18 @@
     .columns.box
         .infos.column.is-5
             h3.subtitle 一般社団法人イトナブ石巻
-            .access
-                p 〒986-0822
-                p 宮城県石巻市中央2-10-21サトミビル1F
-                p （石巻駅から徒歩10分）
-                .icon-text
-                    b-icon(icon='phone')
-                    span 0225-90-4282
-                .icon-text
-                    b-icon(icon='email')
-                    span info@itnav.jp
-                .icon-text
-                    b-icon(icon='home')
-                    a(href='http://itnav.jp') http://itnav.jp
+            p 〒986-0822
+            p 宮城県石巻市中央2-10-21サトミビル1F
+            p （石巻駅から徒歩10分）
+            .icon-text
+                b-icon(icon='phone')
+                span 0225-90-4282
+            .icon-text
+                b-icon(icon='email')
+                span info@itnav.jp
+            .icon-text
+                b-icon(icon='home')
+                a(href='http://itnav.jp') http://itnav.jp
 
         .column.is-7
             google-map(pb='!1m14!1m8!1m3!1d3125.3867434030044!2d141.307541!3d38.432535!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f89a49e39fadcf1%3A0x312a01a95416a16a!2z5pel5pys44CB44CSOTg2LTA4MjIg5a6u5Z-O55yM55-z5be75biC5Lit5aSu77yS5LiB55uu77yR77yQ4oiS77yS77yR!5e0!3m2!1sja!2sus!4v1502275770310')
@@ -50,6 +49,6 @@ export default class ItnavAccess extends Vue {
     .infos > h3
         margin: 1rem 0 4rem 0
 
-    .access > p, .access > .icon-text:not(:last-child)
+    .infos > p, .infos > .icon-text:not(:last-child)
         margin-bottom: 2rem
 </style>
