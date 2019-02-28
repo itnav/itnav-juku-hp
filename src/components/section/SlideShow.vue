40f907ec632cad24c0181329e7bb62f56eb95a6a
TamaRabbtis <crowncrown18@yahoo.co.jp>
2019-02-28 20:27:38 +0900

    スライドショーをswiperで作り直した

--- src/components/section/SlideShow.vue
+++ src/components/section/SlideShow.vue
@@ -1,15 +1,26 @@
 <template lang='pug'>
 .vue-slide-show
-    img.full-image(:src='imgs["photo1"]' :style='imgStyles[0]')
-    img.full-image(:src='imgs["photo2"]' :style='imgStyles[1]')
-    img.full-image(:src='imgs["photo3"]' :style='imgStyles[2]')
-    img.full-image(:src='imgs["photo4"]' :style='imgStyles[3]')
-    img.full-image(:src='imgs["photo5"]' :style='imgStyles[4]')
+    swiper(:options="swiperOption")
+        swiper-slide 
+            img(:src='imgs["photo1"]')
+        swiper-slide
+            img(:src='imgs["photo2"]')
+        swiper-slide
+            img(:src='imgs["photo3"]')
+        swiper-slide
+            img(:src='imgs["photo4"]')
+        swiper-slide
+            img(:src='imgs["photo5"]')
+        .swiper-pagination(slot="pagination")
 </template>
+
 
 <script lang='ts'>
 import { Vue, Component, Watch } from 'vue-property-decorator';
 import VueUtil from '@/scripts/util/VueUtil';
+import { setTimeout } from 'timers';
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 
 /**
  * Vue Component
@@ -17,20 +28,25 @@ import VueUtil from '@/scripts/util/VueUtil';
 @Component
 export default class SlideShow extends Vue {
     protected imgs = {
-        'photo1': require('@/resources/img/photo/image1.jpg'),
+        'photo1': require('@/resources/img/photo/image.jpg'),
         'photo2': require('@/resources/img/photo/image2.jpg'),
         'photo3': require('@/resources/img/photo/image3.jpg'),
         'photo4': require('@/resources/img/photo/image4.jpg'),
         'photo5': require('@/resources/img/photo/image5.jpg'),
-    } as Imgs;
+
+    } as Imgs;  
 
     protected currentImgIndex = 0;
-    protected get imgStyles(): any[] {
-        return Object.keys(this.imgs).map((img, i) => {
-            const offset = (i - this.currentImgIndex) * 100;
-            return { transform: `translateX(${offset}%)` };
-        });
-    }
+
+    protected swiperOption = {
+        pagination : {
+            el : '.swiper-pagination'
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+          },
+    };
 
     @Watch('currentImgIndex')
     protected checkValidation(): void {
@@ -40,7 +56,8 @@ export default class SlideShow extends Vue {
                 > Object.keys(this.imgs).length - 1) {
             this.currentImgIndex = 0
         }
-    }
+    } 
+
 }
 </script>
 
@@ -52,6 +69,7 @@ export default class SlideShow extends Vue {
 
     img
         transition: all 0.5s ease-out
+        
 
         &:not(:first-child)
             position: absolute
