cac4f8f4440cb13b22b68d13e4ba6744319b133d
TamaRabbtis <crowncrown18@yahoo.co.jp>
2019-02-28 20:26:52 +0900

    Support.vueを作った

---
+++ src/components/section/Support.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+.vue-support
+    h2.title.has-text-centered 〜 遊びから学びへ 〜
+    p.has-text-centered aaaaaaaaaaaaaaaaaaaaaaa
+    
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import { GlobalPositions } from 'buefy/types/helpers';
+import VueUtil from '@/scripts/util/VueUtil';
+
+
+/**
+ * Vue Component
+ * TODO: rename to StafIntro
+ */
+@Component
+export default class Support extends Vue {
+
+}
+</script>
+
+<style lang='sass' scoped>
+@import 'entry/variable'
+
+.vue-support
+    
+</style>
